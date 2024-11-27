88c221e54cbaf685ea1c933a943e70707dee042b
Ali AL-Ward <aliabdullahalward@gmail.com>
2024-11-28 00:05:40 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,353 @@
+
+<?php
+include_once('hms/include/config.php');
+if(isset($_POST['submit']))
+{
+$name=$_POST['fullname'];
+$email=$_POST['emailid'];
+$mobileno=$_POST['mobileno'];
+$dscrption=$_POST['description'];
+$query=mysqli_query($con,"insert into tblcontactus(fullname,email,contactno,message) value('$name','$email','$mobileno','$dscrption')");
+echo "<script>alert('هل انت متاكد من جميع بياناتك');</script>";
+echo "<script>window.location.href ='index.php'</script>";
+
+} ?>
+<!doctype html>
+<html >
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title> مستشفى نورا التخصصي </title>
+
+    <link rel="shortcut icon" href="assets/images/fav.jpg">
+    <link rel="stylesheet" href="assets/css/bootstrap-rtl.min.css">
+
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/fontawsom-all.min.css">
+     <link rel="stylesheet" href="assets/css/animate.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css" />
+    
+</head>
+
+    <body>
+
+    <!-- ################# Header Starts Here#######################--->
+    
+      <header id="menu-jk">
+    
+        <div id="nav-head" class="header-nav">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-2 col-md-3  col-sm-12" style="color:#000;font-weight:bold; font-size:42px; margin-top: 1% !important;">NORA|HOSPITAL
+                       <a data-toggle="collapse" data-target="#menu" href="#menu" ><i class="fas d-block d-md-none small-menu fa-bars"></i></a>
+                    </div>
+                    <div id="menu" class="col-lg-8 col-md-9 d-none d-md-block nav-item">
+                        <ul>
+                            <li><a href="#contact_us">اتصل بنا</a></li>
+                            <li><a href="#gallery">عن المستشفى</a></li>
+
+                            <li><a href="#services">الاقسام</a></li>
+
+                            <li><a href="#">الرئيسية</a></li>
+
+                        </ul>
+                    </div>
+                   
+                </div>
+
+            </div>
+        </div>
+    </header>
+    
+     <!-- ################# Slider Starts Here#######################--->
+
+    <div class="slider-detail">
+
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            </ol>
+
+
+   
+
+
+            <div class="carousel-inner">
+                <div class="carousel-item ">
+                    <img class="d-block w-100" src="assets/images/slider/slider_2.jpg" alt="Second slide">
+                    <div class="carousel-cover"></div>
+                    <div class="carousel-caption vdg-cur d-none d-md-block">
+                        <h5 class="animated bounceInDown">مستشفى نورا التخصصي</h5>
+            
+                         
+                    
+                    </div>
+                </div>
+                
+                <div class="carousel-item active">
+                    <img class="d-block w-100" src="assets/images/slider/slider_3.jpg" alt="Third slide">
+                      <div class="carousel-cover"></div>
+                    <div class="carousel-caption vdg-cur d-none d-md-block">
+                        <h5 class="animated bounceInDown">مستشفى نورا التخصصي</h5>
+            
+                         
+                    
+                    </div>
+              
+                </div>
+                
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+
+
+    </div>
+    
+  
+
+    <!-- ################# Our Departments Starts Here#######################--->
+
+
+    <section id="services" class="key-features department">
+        <div class="container">
+            <div class="inner-title">
+
+                <h2>الاقسام التخصصية للمستشفى</h2>
+            </div>
+
+            <div class="row">
+                <div class="col-lg-4 col-md-6">
+                    <div class="single-key">
+                        <i class="fas fa-heartbeat"></i>
+                        <h5>طب القلب</h5>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6">
+                    <div class="single-key">
+                        <i class="fas fa-ribbon"></i>
+                        <h5>طب العظام</h5>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6">
+                    <div class="single-key">
+                       <i class="fab fa-monero"></i>
+                        <h5>طب الاعصاب</h5>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6">
+                    <div class="single-key">
+                        <i class="fas fa-capsules"></i>
+                        <h5>خط انابيب الادوية</h5>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6">
+                    <div class="single-key">
+                        <i class="fas fa-prescription-bottle-alt"></i>
+                        <h5>فريق الصيدلة</h5>
+                    </div>
+                </div>
+
+
+
+                <div class="col-lg-4 col-md-6">
+                    <div class="single-key">
+                        <i class="far fa-thumbs-up"></i>
+                        <h5>ادوية عالية الجودة</h5>
+
+                    </div>
+                </div>
+            </div>
+
+
+
+
+
+
+        </div>
+
+    </section>
+    
+    
+  
+    
+   
+    
+            <!--  ************************* Gallery Starts Here ************************** -->
+        <div id="gallery" class="gallery">    
+           <div class="container">
+              <div class="inner-title">
+
+                <h2>عن المستشفى</h2>
+            </div>
+              <div class="row">
+                
+
+        <div class="gallery-filter d-none d-sm-block">
+            <button class="btn btn-default filter-button" data-filter="all">الجميع</button>
+            <button class="btn btn-default filter-button" data-filter="hdpe">طب الاسنان</button>
+            <button class="btn btn-default filter-button" data-filter="sprinkle">طب القلب</button>
+            <button class="btn btn-default filter-button" data-filter="spray"> طب الاعصاب</button>
+            <button class="btn btn-default filter-button" data-filter="irrigation">المختبر</button>
+        </div>
+        <br/>
+
+
+
+            <div class="gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-6 filter hdpe">
+                <img src="assets/images/gallery/gallery_01.jpg" class="img-responsive">
+            </div>
+
+            <div class="gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-6 filter sprinkle">
+                <img src="assets/images/gallery/gallery_02.jpg" class="img-responsive">
+            </div>
+
+            <div class="gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-6 filter hdpe">
+                <img src="assets/images/gallery/gallery_03.jpg" class="img-responsive">
+            </div>
+
+            <div class="gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-6 filter irrigation">
+                <img src="assets/images/gallery/gallery_04.jpg" class="img-responsive">
+            </div>
+
+            <div class="gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-6 filter spray">
+                <img src="assets/images/gallery/gallery_05.jpg" class="img-responsive">
+            </div>
+
+          
+
+            <div class="gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-6 filter spray">
+                <img src="assets/images/gallery/gallery_06.jpg" class="img-responsive">
+            </div>
+
+        </div>
+    </div>
+       
+       
+       </div>
+        <!-- ######## Gallery End ####### -->
+    
+    
+     <!--  ************************* Contact Us Starts Here ************************** -->
+    
+    <section id="contact_us" class="contact-us-single">
+        <div class="row no-margin">
+
+            <div  class="col-sm-12 cop-ck">
+                <form method="post">
+                <h2 >تواصل معنا</h2>
+                    <div class="row cf-ro">
+                        <div  class="col-sm-3"><label>الاسم الرباعي :</label></div>
+                        <div class="col-sm-8"><input type="text" placeholder="ادخل الاسم" name="fullname" class="form-control input-sm" required ></div>
+                    </div>
+                    <div  class="row cf-ro">
+                        <div  class="col-sm-3"><label>البريد الالكتروني :</label></div>
+                        <div class="col-sm-8"><input type="text" name="emailid" placeholder="ادخل البريد الالكتروني" class="form-control input-sm"  required></div>
+                    </div>
+                     <div  class="row cf-ro">
+                        <div  class="col-sm-3"><label>رقم الهاتف:</label></div>
+                        <div class="col-sm-8"><input type="text" name="mobileno" placeholder="ادخل رقم الهاتف" class="form-control input-sm" required ></div>
+                    </div>
+                     <div  class="row cf-ro">
+                        <div  class="col-sm-3"><label>محتوى الرسالة:</label></div>
+                        <div class="col-sm-8">
+                          <textarea rows="5" placeholder="ادخل محتوى الرسالة" class="form-control input-sm" name="description" required></textarea>
+                        </div>
+                    </div>
+                     <div  class="row cf-ro">
+                        <div  class="col-sm-3"><label></label></div>
+                        <div class="col-sm-8">
+                         <button class="btn btn-success btn-sm" type="submit" name="submit">ارسال</button>
+                        </div>
+                </div>
+            </form>
+            </div>
+     
+        </div>
+    </section>
+    
+    
+    
+    
+    
+    <!-- ################# Footer Starts Here#######################--->
+
+
+    <footer class="footer">
+        <div class="container">
+            <div class="row">
+       
+                <div class="col-md-6 col-sm-12">
+                    <ul class="list-unstyled link-list">
+                        <li><a ui-sref="portfolio" href="#services">الاقسام التخصصصية</a><i class="fa fa-angle-right"></i></li>
+                        <li><a ui-sref="gallery" href="#gallery">عن المستشفى</a><i class="fa fa-angle-right"></i></li>
+                        <li><a ui-sref="contact" href="#contact">اتصل بنا </a><i class="fa fa-angle-right"></i></li>
+                    </ul>
+                </div>
+                <div  class="col-md-6 col-sm-12 map-img">
+                    <h2 style="float:right">العنوان</h2>
+                    <P style="float:right">اليمن - صنعاء - شارع زايد</P>
+                    <h2 style="float:right">اتصل بنا عبر الارقام التالية</h2>
+                    <P style="float:right">01-445566 | 01447766 | +967 774087409  </P>
+
+                    <address class="md-margin-bottom-40">
+
+<?php
+$ret=mysqli_query($con,"select * from tblpage where PageType='contactus' ");
+while ($row=mysqli_fetch_array($ret)) {
+?>
+
+
+                        <?php  echo $row['PageDescription'];?> <br>
+                        Phone: <?php  echo $row['MobileNumber'];?> <br>
+                        Email: <a href="mailto:<?php  echo $row['Email'];?>" class=""><?php  echo $row['Email'];?></a><br>
+                        Timing: <?php  echo $row['OpenningTime'];?>
+                    </address>
+
+        <?php } ?>
+
+
+
+
+
+                </div>
+            </div>
+        </div>
+        
+
+    </footer>
+    <div class="copy">
+            <div class="container">
+          تم تطوير الموقع بواسطة م/علي الورد للتواصل 774087409
+                
+     
+            </div>
+
+        </div>
+    
+    </body>
+
+<script src="assets/js/jquery-3.2.1.min.js"></script>
+<script src="assets/js/popper.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+<script src="assets/plugins/scroll-nav/js/jquery.easing.min.js"></script>
+<script src="assets/plugins/scroll-nav/js/scrolling-nav.js"></script>
+<script src="assets/plugins/scroll-fixed/jquery-scrolltofixed-min.js"></script>
+
+<script src="assets/js/script.js"></script>
+
+
+
+</html>
